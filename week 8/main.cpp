17fad363ded1a31eb2e4b97221588530d85e1778
sebin <sebin4548@gmail.com>
2023-05-08 20:47:22 +0900

    ..

--- week 8/main.cpp
+++ week 8/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include<iomanip>
 #include <cmath>
 //#include "fibo.cpp"
 using namespace std;
@@ -40,9 +41,122 @@ int Fibonacci_non_recursive(int n){
     }
     return temp;
 }
+int NumPaths(int row, int col, int n){
+    if((row ==n) || (col ==n)) return 1;
+    else return NumPaths(row+1, col, n) + NumPaths(row, col + 1, n);
+}
+int mat[10][10];
+int NumPaths_adv(int row, int col, int n){
+    if(mat[row][col] != 0){
+        return mat[row][col];
+    }else{
+        if((row ==n) || (col ==n)) {
+            mat[row][col] = 1;
+            return 1;
+        }
+        else {
+            
+            mat[row][col] = NumPaths_adv(row+1, col, n) + NumPaths_adv(row, col + 1, n);
+            return mat[row][col];
+        }
+    }
+    
+}
+class Node{
+public:
+    int value;
+    Node* next;
+};
+class UnsortedType{
+    
+public:
+    Node* First;
+    UnsortedType(){
+        First = new Node();
+        
+        First->next = NULL;
+    }
+    Node* MinLoc(Node* location, Node* minNode){
+        if(location != NULL){
+            if(location->value < minNode->value){
+                return MinLoc(location->next, location);
+            }
+            else{
+                return MinLoc(location->next, minNode);
+            }
+        }else return minNode;
+    }
+    Node* MinLOC(Node* location){
+        if(location == NULL){
+            return NULL;
+        }
+        else if(location->next == NULL){
+            return location;
+        }
+        else{
+            Node* minPtr = MinLOC(location->next);
+            if(location->value < minPtr->value){
+                return location;
+            }
+            return minPtr;
+        }
+    }
+    void Sort(Node *location){
+        Node* minPtr;
+        int temp;
+        if(location != NULL){
+            minPtr =  MinLoc(location, location);
+            temp = minPtr->value;
+            minPtr->value = location ->value;
+            location -> value = temp;
+            Sort(location ->next);
+        }
+    }
+    void Enque(int x){
+        Node* current = First;
+        while(current->next != NULL){
+            current = current -> next;
+        }
+        Node* en = new Node();
+        en -> value = x;
+        en -> next = NULL;
+        current->next = en;
+    }
+    void Print(){
+        Node* current = First;
+        while(current->next != NULL){
+            cout<<current->next-> value;
+            current = current -> next;
+        }
+    }
+};
+int SumSquares_a(Node* list){
+    if(list->next != NULL){
+        return SumSquares_a(list->next) + (list->value)*(list->value);
+    }else{
+        return (list->value)*(list->value);
+    }
+}
 int main(){
     cout<<Fibonacci_recursive(10)<<endl;
     cout<<Fibonacci_non_recursive(10)<<endl;
     cout<<SqrRoot(9, 0.1, 0.01)<<endl;
-    cout<<SqrRoot(9, 0.1, 0.01);
+    cout<<SqrRoot(9, 0.1, 0.01)<<endl;
+    cout<<NumPaths(1, 1, 6)<<endl;
+    cout<<NumPaths_adv(1, 1, 6)<<endl;
+
+    UnsortedType x;
+    x.Enque(10);
+    x.Enque(2);
+    x.Enque(3);
+    x.Enque(4);
+    x.Enque(5);
+    x.Print();
+    Node* k = x.MinLoc(x.First->next, x.First->next);
+    Node* l = x.MinLOC(x.First->next);
+    cout<<endl<<k->value<<endl;
+    cout<<endl<<l->value<<endl;
+//    cout<<endl<<SumSquares_a(x.First);
+    x.Sort(x.First->next);
+    x.Print();
 }
