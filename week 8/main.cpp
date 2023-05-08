86f51305869b7e8d6623005c254ed1537193f047
sebin <sebin4548@gmail.com>
2023-05-08 15:23:09 +0900

    Initial Commit

---
+++ week 8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  week 8
+//
+//  Created by KWON SEBIN on 2023/05/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
