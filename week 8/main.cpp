c65ccee4f677c4d399b775a5fded888c44fbea4c
sebin <sebin4548@gmail.com>
2023-05-08 15:25:36 +0900

    으악

--- week 8/main.cpp
+++ week 8/main.cpp
@@ -6,9 +6,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout<<"sssss";
     return 0;
 }
